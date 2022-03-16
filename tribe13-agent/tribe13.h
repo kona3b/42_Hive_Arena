e19eff827558ee2abc2bb20619286f870ba2f20a
Lauri Salmi <lasalmi@c2r3p13.hive.fi>
2022-03-16 12:08:08 +0200

    Added macros to header

--- tribe13-agent/tribe13.h
+++ tribe13-agent/tribe13.h
@@ -6,3 +6,13 @@
 
 int	check_if_type(agent_info_t info, int dir, int type);
 command_t back_to_hive(agent_info_t info);
+#define EMPTY 0
+#define B0 1
+#define B1 2
+#define B0_WF 3
+#define B1_WF 4
+#define FLOWER 5
+#define WALL 6
+#define HIVE_0 7
+#define HIVE_1 8
+#define OUT 9
